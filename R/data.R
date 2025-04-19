793b1aaf9758dd614b091f68e116d3ebd5295daf
Brandan Rosa <brandanrosa22@gmail.com>
2025-04-19 10:15:19 -0500

    Initial

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' EPA Rivers and Streams Data
+#'
+#' From the EPA National Rivers and Streams Assessment 2018-2019
+#'
+#' @format
+#' A data frame 290 observations and 8 variables:
+#' \describe{ **EDIT FIELDS BELOW FOR VARIABLES**
+#'   \item{epaRegion}{The EPA Region in which the fish composite sample was collected}
+#'   \item{state}{U.S. Postal Service 2-letter abbreviation for the state in which the fish composite sample was collected}
+#'   \item{epaSampID}{Unique 6-digit number assigned by EPA to each fish composite sample}
+#'   \item{amtMERC}{Concentration of the chemical}
+#'   \item{amtPCB}{Concentration of the chemical}
+#'   \item{amtPFA}{Concentration of the chemical}
+#'   \item{species}{Generally accepted common name based on Nelson et al. (2004)}
+#'   \item{feeder}{Classification as P=Predator or BD=Bottom-Dweller}
+#'   ...
+#' }
+#' @source <https://riverstreamassessment.epa.gov/webreport/>
+"data"
